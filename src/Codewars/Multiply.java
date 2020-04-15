dd9e853e648e501ed0b92c4d625afadc404f47ed
olenafrediuk <frediuk_olena@ukr.net>
2020-04-15 20:08:10 +0300

    8 kyu - Multiply

---
+++ src/Codewars/Multiply.java
@@ -0,0 +1,15 @@
+// Multiply
+
+/*
+The code does not execute properly. Try to figure out why.
+BUGS
+ */
+
+
+package Codewars;
+
+public class Multiply {
+    public static Double multiply(Double a, Double b) {
+        return a * b;
+    }
+}
