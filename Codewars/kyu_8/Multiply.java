a8d8904b9ce700a36fc83fe615c826a47f532bef
olenafrediuk <frediuk_olena@ukr.net>
2020-04-16 02:49:15 +0300

    altERnaTIng cAsE <=> ALTerNAtiNG CaSe

--- Codewars/kyu_8/Multiply.java
+++ Codewars/kyu_8/Multiply.java
@@ -4,9 +4,7 @@
 The code does not execute properly. Try to figure out why.
 BUGS
  */
-
-
-package Codewars;
+//package kyu_8;
 
 public class Multiply {
     public static Double multiply(Double a, Double b) {
---
+++ Codewars/kyu_8/altERnaTIngcAsE.java
@@ -0,0 +1,56 @@
+// altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+
+/*
+altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+
+Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+
+StringUtils.toAlternativeString("hello world") == "HELLO WORLD"
+StringUtils.toAlternativeString("HELLO WORLD") == "hello world"
+StringUtils.toAlternativeString("hello WORLD") == "HELLO world"
+StringUtils.toAlternativeString("HeLLo WoRLD") == "hEllO wOrld"
+StringUtils.toAlternativeString("12345") == "12345" // Non-alphabetical characters are unaffected
+StringUtils.toAlternativeString("1a2b3c4d5e") == "1A2B3C4D5E"
+StringUtils.toAlternativeString("StringUtils.toAlternatingCase") == "sTRINGuTILS.TOaLTERNATINGcASE"
+As usual, your function/method should be pure, i.e. it should not mutate the original string.
+FUNDAMENTALS
+ */
+
+//package kyu_8;
+
+public class StringUtils {
+
+    public static String toAlternativeString(String str) {
+        String out = "";
+        for (int i = 0; i < str.length(); i++) {
+            Character c = str.charAt(i);
+            if (c == Character.toLowerCase(c)) {
+                out += Character.toUpperCase(c);
+            } else {
+                out += Character.toLowerCase(c);
+            }
+            System.out.println(out);
+        }
+        return out;
+    }
+
+}
+
+// or
+
+public class StringUtils {
+
+    public static String toAlternativeString(String string) {
+        String[] arr = string.split("");
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == arr[i].toLowerCase()) {
+                arr[i] = arr[i].toUpperCase();
+            } else {
+                arr[i] = arr[i].toLowerCase();
+            }
+        }
+
+        return String.join("", arr);
+    }
+}
