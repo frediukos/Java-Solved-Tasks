c77d4b4f902c5b82ba5b5389fc74bad54fd7194d
olenafrediuk <frediuk_olena@ukr.net>
2020-05-02 23:43:11 +0300

    All Star Code Challenge #18

---
+++ Codewars/kyu_8/YouCantCodeUnderPressure1.java
@@ -0,0 +1,13 @@
+// You Can't Code Under Pressure #1
+
+/*
+Code as fast as you can! You need to double the integer and return it.
+FUNDAMENTALS   NUMBERS   INTEGERS
+ */
+//package kyu_8;
+
+class Java {
+    public static int doubleInteger(int i) {
+        return i * 2;
+    }
+}
