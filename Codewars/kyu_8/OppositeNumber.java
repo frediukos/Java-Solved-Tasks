430e53457422364f1fba37c2f5d8cf35e7f7ac3e
olenafrediuk <frediuk_olena@ukr.net>
2020-05-03 02:29:02 +0300

    Opposite number

---
+++ Codewars/kyu_8/OppositeNumber.java
@@ -0,0 +1,22 @@
+// Opposite number
+
+/*
+Very simple, given a number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+FUNDAMENTALS
+ */
+
+//package kyu_8;
+
+public class Kata
+{
+    public static int opposite(int number)
+    {
+        return -number;
+    }
+}
