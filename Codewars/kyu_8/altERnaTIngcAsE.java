36c7e29e8bbb29898a282882e2c5fd1bcb6c3e1a
olenafrediuk <frediuk_olena@ukr.net>
2020-05-02 23:45:17 +0300

    altERnaTIng cAsE <=> ALTerNAtiNG CaSe

--- Codewars/kyu_8/altERnaTIngcAsE.java
+++ Codewars/kyu_8/altERnaTIngcAsE.java
@@ -3,7 +3,10 @@
 /*
 altERnaTIng cAsE <=> ALTerNAtiNG CaSe
 
-Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+Define String.prototype.toAlternatingCase
+(or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language;
+see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes
+lowercase. For example:
 
 StringUtils.toAlternativeString("hello world") == "HELLO WORLD"
 StringUtils.toAlternativeString("HELLO WORLD") == "hello world"
